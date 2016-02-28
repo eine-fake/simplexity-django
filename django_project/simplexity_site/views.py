d6bf6b9c969250cff4746bc0e2d6886c80b4b7b6
eine fake <eine.fake.email.address@gmail.com>
2016-02-28 12:09:28 -0500

    deleted simplexity_site app

--- django_project/simplexity_site/views.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-
-# Create your views here.
-
-def index(request):
-	return HttpResponse("Hi Kory & Marketa! My name is Simplexity, and I am online.")
