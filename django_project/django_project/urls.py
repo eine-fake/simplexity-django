f75d84ffb7d225e51b533672c29012f98c4b41a7
eine fake <eine.fake.email.address@gmail.com>
2016-02-28 12:29:37 -0500

    modified urls.py(s)

--- django_project/django_project/urls.py
+++ django_project/django_project/urls.py
@@ -1,13 +1,9 @@
 from django.conf.urls import patterns, include, url
+from simplexity_site import views
 from django.contrib import admin
-
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'django_project.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
-
-	url(r'^simplexity_site/', include('simplexity_site.urls')),
-	url(r'^admin/', include(admin.site.urls)),
+    url(r'^$', views.index, name='index'),
+    url(r'^admin/', include(admin.site.urls)),
 )
